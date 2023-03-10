5f26f30145a64ae3d0ba19d801c40de6d4a05e1e
Abdelrhman_Elnainay <abdo.ahmed022@gmail.com>
2023-03-10 08:38:05 +0200

    java classes generated from proto file

---
+++ trending-movies-service/src/main/java/com/moviecatalogservice/services/TrendingMoviesService.java
@@ -0,0 +1,15 @@
+package com.moviecatalogservice.services;
+
+import com.TrendingMoviesClientService.RatingList;
+import com.TrendingMoviesClientService.TrendingMoviesIdsServiceGrpc;
+import com.TrendingMoviesClientService.request;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TrendingMoviesService extends TrendingMoviesIdsServiceGrpc.TrendingMoviesIdsServiceImplBase {
+
+    @Override
+    public void getTrendingMoviesIds(request request, io.grpc.stub.StreamObserver<RatingList> responseObserver) {
+
+    }
+}
